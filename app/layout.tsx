00be5bb5cf89ae3fe0482ff996b6b2e9f7b13c7d
Fadhil Musyaffa <musyaffa262@gmail.com>
2025-07-17 19:08:14 +0700

    feat fixed layout & added login

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,28 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { AppLayout } from "@/components/app-layout"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { AppLayout } from "@/components/app-layout";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "QIU System - Inventory Management",
   description: "Sistem manajemen inventory yang powerful dan mudah digunakan",
-    generator: 'v0.dev'
-}
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="id">
       <body className={inter.className}>
-        <AppLayout>{children}</AppLayout>
+        {/* <AppLayout>{children}</AppLayout> */}
+        {children}
       </body>
     </html>
-  )
+  );
 }
---
+++ app/login/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import React, { useState } from "react";
+import logo from "@/public/logo.png";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <section>
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <Card className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
+          <div className="p-6 space-y-2 md:space-y-2 sm:p-8">
+            <Image
+              src={logo}
+              width={44}
+              height={44}
+              alt="logo"
+              className="w-[5rem] "
+            />
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Selamat Datang
+            </h1>
+            <p className="text-black/60">Login untuk mengakses QIU Sistem</p>
+            <form className="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label
+                  htmlFor="username"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Username
+                </label>
+                <Input
+                  type="text"
+                  name="username"
+                  id="username"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Masukkan Username"
+                />
+              </div>
+              <div>
+                <div className="grid gap-3">
+                  <Label htmlFor="password">Password</Label>
+                  <div className="relative">
+                    <Input
+                      id="password"
+                      type={showPassword ? `text` : `password`}
+                      placeholder="*******"
+                    />
+                    <div
+                      onClick={() => setShowPassword(!showPassword)}
+                      className="absolute right-4 bottom-2 cursor-pointer hover:scale-[105%] focus:text"
+                    >
+                      {!showPassword ? (
+                        <EyeIcon className="size-5" />
+                      ) : (
+                        <EyeOffIcon className="size-5" />
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <Button type="submit" className="flex w-full">
+                Masuk
+              </Button>
+            </form>
+          </div>
+        </Card>
+      </div>
+    </section>
+  );
+};
+
+export default LoginPage;
