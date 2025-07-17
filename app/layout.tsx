71ed4b8a1a2327573b0f0c22d6b7c52a587d24a6
Fadhil Musyaffa <musyaffa262@gmail.com>
2025-07-17 19:11:07 +0700

    feat  added logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AppLayout } from "@/components/app-layout";
+import logo from "@/public/logo.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="id">
+      <head>
+        <link rel="icon" href="/logo.png" sizes="any" />
+      </head>
       <body className={inter.className}>
         {/* <AppLayout>{children}</AppLayout> */}
         {children}
