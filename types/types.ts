2ccbfb4850126ab2c5df497c246161c459a56f15
Fadhil Musyaffa <musyaffa262@gmail.com>
2025-07-28 22:58:02 +0700

    feat warehouse page to have fully functional things

--- types/types.ts
+++ types/types.ts
@@ -1,51 +1,47 @@
 import carouselone from "@/public/carouselone.jpg";
-import {StaticImageData} from "next/image";
+import { StaticImageData } from "next/image";
 
 export interface Customer {
-    id: string;
-    name: string;
-    code: string;
-    address: string;
-    currency: string;
-    top: string;
-    status: "active" | "inactive";
+  id: string;
+  name: string;
+  code: string;
+  address: string;
+  currency: string;
+  top: string;
+  status: "active" | "inactive";
 }
-
 
 export interface Warehouse {
-    id: string;
-    name: string;
-    address: string;
-    isActive: boolean;
+  id: number;
+  name: string;
+  address: string;
+  is_active: boolean;
 }
-
 
 export interface Product {
-    id: string;
-    nama: string;
-    SKU: string;
+  id: string;
+  nama: string;
+  SKU: string;
 
-    status: "active" | "inactive";
-    jumlah: number;
-    harga: number;
-    satuan: string;
-    vendor: string;
-    gambar: (StaticImageData | string)[];
-    kategori1: string;
-    kategori2: string;
+  status: "active" | "inactive";
+  jumlah: number;
+  harga: number;
+  satuan: string;
+  vendor: string;
+  gambar: (StaticImageData | string)[];
+  kategori1: string;
+  kategori2: string;
 }
 
-
 export interface Category {
-    id: string;
-    name: string;
-    status: boolean;
+  id: string;
+  name: string;
+  status: boolean;
 }
 
 export interface Unit {
-    id: string;
-    name: string;
-    symbol: string;
-    status: boolean;
+  id: string;
+  name: string;
+  symbol: string;
+  status: boolean;
 }
-
