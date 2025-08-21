feae2e77ccae3c1381d9ab91f194e1e60dd1d667
Ader-EX <musyaffa262@gmail.com>
2025-08-21 10:31:05 +0700

    fix : vendor in Item shouldnt exist

--- components/Product/ProductTableView.tsx
+++ components/Product/ProductTableView.tsx
@@ -1,111 +1,111 @@
-import { Card, CardContent } from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import { MoreHorizontal, Eye, Edit, Trash2 } from "lucide-react";
+import {Card, CardContent} from "@/components/ui/card";
+import {Badge} from "@/components/ui/badge";
+import {Button} from "@/components/ui/button";
+import {MoreHorizontal, Eye, Edit, Trash2} from "lucide-react";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
 } from "@/components/ui/table";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Item } from "@/types/types";
+import {Item} from "@/types/types";
 
 interface ProductTableViewProps {
-  products?: Item[];
-  onEdit?: (item: Item) => void;
-  onDelete?: (id: number) => Promise<void>;
-  onView?: (product: Item) => void;
+    products?: Item[];
+    onEdit?: (item: Item) => void;
+    onDelete?: (id: number) => Promise<void>;
+    onView?: (product: Item) => void;
 }
 
 export default function ProductTableView({
-  products,
-  onEdit,
-  onDelete,
-  onView,
-}: ProductTableViewProps) {
-  const paginatedProducts = products;
+                                             products,
+                                             onEdit,
+                                             onDelete,
+                                             onView,
+                                         }: ProductTableViewProps) {
+    const paginatedProducts = products;
 
-  return (
-    <Card>
-      <CardContent className="p-0 min-w-[900px]">
-        <Table>
-          <TableHeader>
-            <TableRow className="bg-gray-50">
-              <TableHead className="font-semibold">Kode Item</TableHead>
-              <TableHead className="font-semibold">Item</TableHead>
-              <TableHead className="font-semibold">Jumlah Unit</TableHead>
+    return (
+        <Card>
+            <CardContent className="p-0 min-w-[900px]">
+                <Table>
+                    <TableHeader>
+                        <TableRow className="bg-gray-50">
+                            <TableHead className="font-semibold">Kode Item</TableHead>
+                            <TableHead className="font-semibold">Item</TableHead>
+                            <TableHead className="font-semibold">Jumlah Unit</TableHead>
 
-              <TableHead className="font-semibold">Satuan</TableHead>
-              <TableHead className="font-semibold">Harga Jual (Rp)</TableHead>
-              <TableHead className="font-semibold">Vendor</TableHead>
-              <TableHead className="font-semibold">Status</TableHead>
-              <TableHead className="text-right font-semibold">Aksi</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {paginatedProducts?.map((product) => (
-              <TableRow key={product.code} className="hover:bg-gray-50">
-                <TableCell className="font-medium">{product.code}</TableCell>
-                <TableCell className="font-medium">{product.name}</TableCell>
-                <TableCell>{product.total_item}</TableCell>
+                            <TableHead className="font-semibold">Satuan</TableHead>
+                            <TableHead className="font-semibold">Harga Jual (Rp)</TableHead>
 
-                <TableCell>{product?.satuan_rel?.name}</TableCell>
-                <TableCell className="font-semibold">{product.price}</TableCell>
-                <TableCell>{product?.vendor_rel?.name || "-"}</TableCell>
-                <TableCell>
-                  <Badge variant={product.is_active ? "okay" : "secondary"}>
-                    {product.is_active ? "Aktif" : "Tidak Aktif"}
-                  </Badge>
-                </TableCell>
-                <TableCell className="text-right">
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <Button variant="ghost" className="h-8 w-8 p-0">
-                        <MoreHorizontal className="h-4 w-4" />
-                      </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                      <DropdownMenuItem
-                        onClick={() => {
-                          if (product && onView) onView(product);
-                        }}
-                      >
-                        <Eye className="mr-2 h-4 w-4" />
-                        Lihat Detail
-                      </DropdownMenuItem>
-                      <DropdownMenuItem
-                        onClick={() => {
-                          if (product && onEdit) onEdit(product);
-                        }}
-                      >
-                        <Edit className="mr-2 h-4 w-4" />
-                        Edit
-                      </DropdownMenuItem>
-                      <DropdownMenuItem
-                        onClick={() => {
-                          if (product && onDelete) onDelete(product.id);
-                        }}
-                        className="text-red-600"
-                      >
-                        <Trash2 className="mr-2 h-4 w-4" />
-                        Hapus
-                      </DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </CardContent>
-    </Card>
-  );
+                            <TableHead className="font-semibold">Status</TableHead>
+                            <TableHead className="text-right font-semibold">Aksi</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {paginatedProducts?.map((product) => (
+                            <TableRow key={product.code} className="hover:bg-gray-50">
+                                <TableCell className="font-medium">{product.code}</TableCell>
+                                <TableCell className="font-medium">{product.name}</TableCell>
+                                <TableCell>{product.total_item}</TableCell>
+
+                                <TableCell>{product?.satuan_rel?.name}</TableCell>
+                                <TableCell className="font-semibold">{product.price}</TableCell>
+
+                                <TableCell>
+                                    <Badge variant={product.is_active ? "okay" : "secondary"}>
+                                        {product.is_active ? "Aktif" : "Tidak Aktif"}
+                                    </Badge>
+                                </TableCell>
+                                <TableCell className="text-right">
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button variant="ghost" className="h-8 w-8 p-0">
+                                                <MoreHorizontal className="h-4 w-4"/>
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align="end">
+                                            <DropdownMenuItem
+                                                onClick={() => {
+                                                    if (product && onView) onView(product);
+                                                }}
+                                            >
+                                                <Eye className="mr-2 h-4 w-4"/>
+                                                Lihat Detail
+                                            </DropdownMenuItem>
+                                            <DropdownMenuItem
+                                                onClick={() => {
+                                                    if (product && onEdit) onEdit(product);
+                                                }}
+                                            >
+                                                <Edit className="mr-2 h-4 w-4"/>
+                                                Edit
+                                            </DropdownMenuItem>
+                                            <DropdownMenuItem
+                                                onClick={() => {
+                                                    if (product && onDelete) onDelete(product.id);
+                                                }}
+                                                className="text-red-600"
+                                            >
+                                                <Trash2 className="mr-2 h-4 w-4"/>
+                                                Hapus
+                                            </DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </CardContent>
+        </Card>
+    );
 }
